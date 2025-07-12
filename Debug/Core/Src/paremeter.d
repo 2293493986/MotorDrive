c2665dde9998314f97000ae22681cfaade4426b5
SHYang <2293493986@qq.com>
2025-07-12 17:30:52 +0800

    电流环切无感闭环低转速可控制IQ进而控制转速，IQ过大之后电机一直提速至停转

--- Debug/Core/Src/paremeter.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-Core/Src/paremeter.obj: ../Core/Src/paremeter.c
-Core/Src/paremeter.obj: D:/CCS_WorkSpace/Project/MT009_OBJECT/MT009/Core/Inc/paremeter.h
-
-../Core/Src/paremeter.c: 
-D:/CCS_WorkSpace/Project/MT009_OBJECT/MT009/Core/Inc/paremeter.h: 
--- Debug/Core/Src/pid.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-Core/Src/pid.obj: ../Core/Src/pid.c
-Core/Src/pid.obj: D:/CCS_WorkSpace/Project/MT009_OBJECT/MT009/Core/Inc/pid.h
-
-../Core/Src/pid.c: 
-D:/CCS_WorkSpace/Project/MT009_OBJECT/MT009/Core/Inc/pid.h: 
